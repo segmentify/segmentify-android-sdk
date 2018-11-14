9ebee6c03820e23d49f219b2c19f1d5dbb839338
Cem <cem_ayan@outlook.com>
2018-11-14 14:39:10 +0300

    push notification methods added.

--- app/src/main/java/com/segmentify/segmentifysdk/EventActivity.java
+++ app/src/main/java/com/segmentify/segmentifysdk/EventActivity.java
@@ -1,9 +1,20 @@
 package com.segmentify.segmentifysdk;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.segmentify.segmentifyandroidsdk.SegmentifyManager;
+import com.segmentify.segmentifyandroidsdk.model.NotificationModel;
+import com.segmentify.segmentifyandroidsdk.model.NotificationType;
 import com.segmentify.segmentifyandroidsdk.model.PageModel;
 import com.segmentify.segmentifyandroidsdk.model.ProductRecommendationModel;
 import com.segmentify.segmentifyandroidsdk.model.RecommendationModel;
@@ -30,5 +41,27 @@ public class EventActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+
+
+
+        Button subscribeButton = findViewById(R.id.button);
+        subscribeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            NotificationModel model = new NotificationModel();
+            model.setDeviceToken(FirebaseInstanceId.getInstance().getToken());
+            model.setType(NotificationType.PERMISSION_INFO);
+            SegmentifyManager.INSTANCE.sendNotification(model);
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
+++ app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
@@ -8,8 +8,9 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SegmentifyManager.INSTANCE.config(this,"8157d334-f8c9-4656-a6a4-afc8b1846e4c","https://dce1.segmentify.com","segmentify-shop.myshopify.com");
+        SegmentifyManager.INSTANCE.config(this,"93a9a7c4-dc2e-49f7-832e-7c7fc3dabbf9","https://dce-test.segmentify.com","ihalilaltun.me");
         SegmentifyManager.INSTANCE.setSessionKeepSecond(100000);
+        SegmentifyManager.INSTANCE.logStatus(true);
     }
 }
 
