3edc30924273c258989d8381d25122e6ffd296da
ebruersoy <ebruersoy.89@gmail.com>
2020-04-27 10:09:56 +0300

    app config fixed

--- app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
+++ app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
@@ -8,7 +8,7 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SegmentifyManager.INSTANCE.config(this,"3974676b-02e7-4507-bdef-350ef31429fb","https://dce1.segmentify.com","demo.segmentify.com");
+        SegmentifyManager.INSTANCE.config(this,"3974676b-02e7-4507-bdef-350ef31429fb","https://dce1.segmentify.com","demo.segmentify.com","https://dce1.segmentify.com");
         SegmentifyManager.INSTANCE.setSessionKeepSecond(100000);
         SegmentifyManager.INSTANCE.logStatus(true);
     }
