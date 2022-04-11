c0923d24a767e6f54d817372698fc0980ffdc56a
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2022-04-11 10:49:06 +0300

    example app account updated

--- app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
+++ app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
@@ -9,8 +9,8 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         // if you are using push module make sure to call SegmentifyManager.Instance.setPushConfig first
-        SegmentifyManager.INSTANCE.setPushConfig("https://gimli-dev.segmentify.com");
-        SegmentifyManager.INSTANCE.config(this, "ae272bfb-214b-4cdd-b5c4-1dddde09e95e","http://gandalf-dev.segmentify.com","ihalilaltun.me");
+        SegmentifyManager.INSTANCE.setPushConfig("https://gimli-qa.segmentify.com");
+        SegmentifyManager.INSTANCE.config(this, "e4dfb71f-eac8-44b7-8938-ce9cce910cb2","http://gandalf-qa.segmentify.com","segmentify-search-showcase.mybigcommerce.com");
         SegmentifyManager.INSTANCE.setSessionKeepSecond(100000);
         SegmentifyManager.INSTANCE.logStatus(true);
     }
