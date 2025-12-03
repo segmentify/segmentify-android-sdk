87a4a95e82fa1b59b1eb0ffcc4fc5e25ecf8471c
Sarpkaya Ture <sarpkayature@gmail.com>
2025-12-03 15:55:02 +0300

    chore: redo credentials

--- app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
+++ app/src/main/java/com/segmentify/segmentifysdk/MyApp.java
@@ -10,8 +10,8 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         // if you are using push module make sure to call SegmentifyManager.Instance.setPushConfig first
-        SegmentifyManager.INSTANCE.setPushConfig("https://psh2.segmentify.com");
-        SegmentifyManager.INSTANCE.config(this, "92e4211c-33dc-46af-bbff-2b734a92099d","https://per2.segmentify.com","sportive.com.tr");
+        SegmentifyManager.INSTANCE.setPushConfig("https://gimli-qa.segmentify.com");
+        SegmentifyManager.INSTANCE.config(this, "3c9e211a-d049-43d5-aa4a-f98b7e66e482","https://gandalf-qa.segmentify.com","demosfy.com");
         SegmentifyManager.INSTANCE.setSessionKeepSecond(86400);
         SegmentifyManager.INSTANCE.logStatus(true);
     }
